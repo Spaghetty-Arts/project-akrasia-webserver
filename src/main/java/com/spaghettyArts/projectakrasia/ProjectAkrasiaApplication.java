1331d56eb05325699172ac3088d69b26838b9847
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-05-15 11:52:52 +0100

    Base Project

---
+++ src/main/java/com/spaghettyArts/projectakrasia/ProjectAkrasiaApplication.java
@@ -0,0 +1,13 @@
+package com.spaghettyArts.projectakrasia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAkrasiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAkrasiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spaghettyArts/projectakrasia/ProjectAkrasiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaghettyArts.projectakrasia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAkrasiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
